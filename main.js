8f877d7b9fcc752bde1656d589f6ae853e705daa
Julie Zimmer <julie.berthiaume@gmail.comgit config --global user.email julie.berthiaume@gmail.com>
2021-10-02 20:36:11 -0500

    success and failure icons working for the three inputs

---
+++ main.js
@@ -0,0 +1,38 @@
+
+// one-liner arrow function //
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+// these are the function calls stored in a variable //
+let username = id("username"),
+    email = id("email"),
+    password = id("password"),
+    form = id("form"),
+    errorMsg = classes("error"),
+    successIcon = classes("success-icon"),
+    failureIcon = classes("failure-icon");
+
+   form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      engine(username, 0, "Username cannot be blank");
+      engine(email, 1, "Email cannot be blank");
+      engine(password, 2, "Password cannot be blank");
+      
+   });
+
+let engine = (id, serial, message) => {
+   if(id.value === ''){
+      errorMsg[serial].innerHTML = message;
+      // changes opacity so the failure icon can be seen //
+      failureIcon[serial].style.opacity = "1"; 
+      successIcon[serial].style.opacity = "0";
+   }
+   else {
+      errorMsg[serial].innerHTML = "";
+      failureIcon[serial].style.opacity = "0";
+      successIcon[serial].style.opacity = "1";
+   }
+};
+ 
